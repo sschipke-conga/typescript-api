5bea8e9a6677ed3aae304c9cee2f03f04a12b104
Scott <swschipke@gmail.com>
2020-05-20 08:53:15 -0600

    Add shelljs and ts dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import express from "express";
+import path from "path";
 const app = express();
 const port = 8080;
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
   res.send("Hello world!");
